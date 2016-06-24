7097206c404e527d65436d19f385734d20f92660
followfork <xuwei_james@163.com>
2016-06-24 09:04:48 +0800

    配置  ngrok代理  token

---
+++ app.js
@@ -0,0 +1,36 @@
+'use strict'
+
+var Koa = require('koa')
+var sha1 = require('sha1')
+
+var config = {
+	wechat: {
+		appID: '',
+		appSecret: '',
+		token: ''
+	}
+}
+
+var app = new Koa()
+
+app.use(function *(next){
+	console.log(this.query)
+
+	var token = config.wechat.token
+	var signature = this.query.signature
+	var nonce = this.query.nonce
+	var timestamp = this.query.timestamp
+	var echostr = this.query.echostr
+	var str = [token, timestamp, nonce].sort().join('')
+	var sha = sha1(str)
+
+	if(sha === signature) {
+		this.body = echostr + ''
+	} else {
+		this.body = 'wrong'
+	}
+})
+
+app.listen(1234)
+
+console.log('listen on 1234')
