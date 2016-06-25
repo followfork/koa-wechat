6d6928397f0b31a1203058c17a40659ffd12d565
followfork <xuwei_james@163.com>
2016-06-25 12:28:55 +0800

    封装配置中间件

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 'use strict'
 
 var Koa = require('koa')
-var sha1 = require('sha1')
+var wechat = require('./wechat/g')
 
 var config = {
 	wechat: {
@@ -13,23 +13,7 @@ var config = {
 
 var app = new Koa()
 
-app.use(function *(next){
-	console.log(this.query)
-
-	var token = config.wechat.token
-	var signature = this.query.signature
-	var nonce = this.query.nonce
-	var timestamp = this.query.timestamp
-	var echostr = this.query.echostr
-	var str = [token, timestamp, nonce].sort().join('')
-	var sha = sha1(str)
-
-	if(sha === signature) {
-		this.body = echostr + ''
-	} else {
-		this.body = 'wrong'
-	}
-})
+app.use(wechat(config.wechat))
 
 app.listen(1234)
 
